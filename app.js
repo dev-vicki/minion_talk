7d3aeccd1d9a2d1ef8235a070c28c80679f4f220
dev-vicki <vignesh.r1999@gmail.com>
2021-06-04 17:38:59 +0530

    buttonCreated

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnTranslate = document.querySelector("#btn-translate");
+// console.log(btnTranslate);
+btnTranslate.addEventListener("click", function clickEventHandler (){
+  console.log("clicked");
+})
