6e4c5ac0fbaa4ebcea71e4d57a71729f4e7d206a
dev-vicki <vignesh.r1999@gmail.com>
2021-07-24 18:41:33 +0530

    assets added

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-var btnTranslate = document.querySelector("#btn-translate");
+var translateButton = document.querySelector("#btn-translate");
+translateButton.addEventListener("click", buttonClickHandler)
+var translateInput = document.querySelector("#txt-input");
+var translateOutput = document.querySelector("#txt-output");
 
-function clickHandler(){
-  console.log("Clicked!!!");
+var url = "https://api.funtranslations.com/translate/minion.json";
+// var url = "https://api.funtranslations.com/translate/emoji.json";
+
+function buttonClickHandler(event) {
+    console.log("button clicked");
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error occured"))
+
 }
 
-btnTranslate.addEventListener("click", clickHandler);
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
+
+
