0b98735eea9be73405c6cf1829264c1dc62d8210
dev-vicki <vignesh.r1999@gmail.com>
2021-07-24 19:46:15 +0530

    meaningful comments updated in js code

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ translateButton.addEventListener("click", buttonClickHandler)
 var translateInput = document.querySelector("#txt-input");
 var translateOutput = document.querySelector("#txt-output");
 
+// API used to fetch data from server
 var url = "https://api.funtranslations.com/translate/minion.json";
-// var url = "https://api.funtranslations.com/translate/emoji.json";
 
+// when pressed the translate button, this function is called and it takes the input from user and fetches the required data from API and returns the output to user
 function buttonClickHandler(event) {
     console.log("button clicked");
     var input = translateInput.value;
@@ -20,6 +21,7 @@ function buttonClickHandler(event) {
 
 }
 
+// takes and encodes user text string as valid URI with the url given
 function constructURL(inputText) {
     var encodedURI = encodeURI(inputText);
     return `${url}?text=${encodedURI}`;
